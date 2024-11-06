9c336cfa4143263dd92e8b6c80ed9dc216a76ea8
ksatishgit <106835928+ksatishgit@users.noreply.github.com>
2024-11-06 14:53:16 +0530

    Update provider.tf

--- FirstDir/Prod/provider.tf
+++ FirstDir/Prod/provider.tf
@@ -10,5 +10,5 @@ terraform {
  provider "azurerm" {
   features {}
   subscription_id = "073a3f32-95b9-4fac-80cf-a013f0644b61"
-  resource_provider_registrations = none
+  resource_provider_registrations = "none"
 }
